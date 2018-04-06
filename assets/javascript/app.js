addbbb18fe48520eed06a2e438ef8aad2686c659
Admin <admin@admins-mbp-2.lan>
2018-04-06 19:12:20 -0400

    overlay added on photo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,25 @@
+$$(document).ready(function() {
 //Global Variables
 timeLeft = 15;
 correctAnswers = 0;
 incorrectAnswers = 0;
+questionsArray = [];
+answersArray = [];
+correctAnswersArray = [];
+
+//Functions
+function initializeGame() {
+  
+}
+
+
+
+
+
+});
+//Notes
+//on start click, countdown to Start
+//timer function
+//array of questions
+// multiple choice and true/false
+//
