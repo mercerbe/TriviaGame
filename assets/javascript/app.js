bf50dfb954c829a4e44673bb1924f1e34b4a3764
Admin <admin@admins-mbp-2.lan>
2018-04-06 23:52:38 -0400

    still tweaking timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
+
 //Global Variables
 var index = 0
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var totalScore = (correctAnswers * 100) - (incorrectAnswers * 100);//append to an element
-
+var gameTimer;
+var time = 10;
 
 //Question objects
 var questionsArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
@@ -13,7 +15,7 @@ var q1 = {
   answerArray: [],
   answerValue: [true, true, true, false],
   answer: "",
-}
+};
 var q2 = {}
 var q3 = {}
 var q4 = {}
@@ -24,48 +26,51 @@ var q8 = {}
 var q9 = {}
 var q10 = {}
 
+//functions
+function runTimer() {
+  time = 10;
+  clearInterval(gameTimer);
+  setInterval(timeLeft,1000);
+};
+function timeLeft() {
+  $("#timer").html(time + " Seconds Remaining");
+  $("#timer").attr("value", time);
+  time--;
+  if (time === 0){
+    setTimeout(function() {
+      alert("Out Of Time!");
+    }, 500);
+    incorrectAnswers++;
+    document.getElementById("incorrectAnswers").innerHTML = incorrectAnswers;
+    runTimer();
+  };
+};//end of timer
 
+function loadQuestion() {
+  $(".questions").append()
 
-//Functions
+};
+
+function setup() {
+
+};
+
+function grabAnswer() {
+
+};
+
+function showScore() {
+
+};
+
+//GamePlay
 $("#startbtn").on("click", function(){
-  var gameTimer = setInterval(timer, 1000);
-  var timeLeft = 10;
 
-  function timer() {
-    if (timeLeft == 0){
-      clearInterval(gameTimer);
-      setTimeout(function() {
-        alert("Out Of Time!");
-      }, 100);
-      timeLeft == 10;
-      incorrectAnswers++;
-      document.getElementById("incorrectAnswers").innerHTML = incorrectAnswers;
-      gameTimer = setInterval(timer, 1000);
-      timer();
-    }else {
-      $("#timer").html(timeLeft + " Seconds Remaining");
-      $("#timer").attr("value", timeLeft);
-        timeLeft--;
-    }
-  };//end of timer
-timer();
+runTimer();
 
-  function loadQuestion() {
-    $(".questions").append()
 
-  };
+});
 
-  function setup() {
-
-  };
-
-  function grabAnswer() {
-
-  };
-
-  function showScore() {
-
-  }
 });
 
 
@@ -74,8 +79,6 @@ timer();
 
 
 
-
-});
 //Notes
 //on play click, timer starts
 //timer function
