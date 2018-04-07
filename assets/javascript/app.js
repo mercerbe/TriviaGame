f7c9d51a62f3b41b18abe83f287f045cfcfc2361
Admin <mercerbe@gmail.com>
2018-04-07 16:46:36 -0400

    question objects completed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,20 +11,65 @@ var seconds;
 //Question objects
 var questionsArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 var q1 = {
-  question: "",
-  answerArray: [],
-  answerValue: [true, true, true, false],
-  answer: "",
+  question: "What does Todd Packers License plate say?",
+  answerArray: ["PRVRT", "LOL69", "WLHUNG", "BOOB69"],
+  answerTrue: [false, false, true, false],
+  answer: "WLHUNG",
 };
-var q2 = {}
-var q3 = {}
-var q4 = {}
-var q5 = {}
-var q6 = {}
-var q7 = {}
-var q8 = {}
-var q9 = {}
-var q10 = {}
+var q2 = {
+  question: "Where does Ryan work after he gets fired from his corportate job?",
+  answerArray: ["A Mall Kiosk", "A Bowling Alley", "A Fast Food Restaurant", "A Strip Club"],
+  answerTrue: [false, true, false, false],
+  answer: "A Bowling Alley",
+};
+var q3 = {
+  question: "What item of clothing does Michael insist on having dry cleaned?",
+  answerArray: ["Socks", "Ties", "Jeans", "Suit Jacket"],
+  answerTrue: [false, false, true, false],
+  answer: "Jeans",
+};
+var q4 = {
+  question: "Who said,'I really should have a tweeter account?'",
+  answerArray: ["Kevin", "Michael", "Creed", "Dwight"],
+  answerTrue: [false, false, false, true],
+  answer: "Dwight",
+};
+var q5 = {
+    question: "What color shirts do the Scranton Branch employees wear in the episode 'Company Picnic'?",
+    answerArray: ["Red", "Green", "Pink", "Orange"],
+    answerTrue: [false, false, true, false],
+    answer: "Pink",
+};
+var q6 = {
+  question: "What does Michael put in Toby's desk to frame him for drug posessoin?",
+  answerArray: ["Ceaser Salad", "Frisee Salad", "Caprese Salad", "Spinach"],
+  answerTrue: [false, false, true, false],
+  answer: "Caprese Salad",
+};
+var q7 = {
+  question: "What does Andy say when he sees the topless picture of Jan taken in Jamaica?",
+  answerArray: ["Boring. Call me if she rolls over.", "How do I get you out of that picture?", "Wait, is that Jan?", "Yea, Michael! You the man!"],
+  answerTrue: [true, false, false, false],
+  answer: "Boring. Call me if she rolls over.",
+};
+var q8 = {
+  question: "What does Devon dress up as in Season 2 Halloween episode?",
+  answerArray: ["A vampire", "A mummy", "A clown", "None of the Above"],
+  answerTrue: [false, false, false, true],
+  answer: "None of the Above",
+};
+var q9 = {
+  question: "What character does not appear in the pilot episode?",
+  answerArray: ["Creed", "Kevin", "Kelly", "Ryan"],
+  answerTrue: [false, false, true, false],
+  answer: "Kelly",
+};
+var q10 = {
+  question: "Who wins the bronze medal in the episode'Office Olympics?'",
+  answerArray: ["Oscar", "Dwight", "Phyllis", "Jim"],
+  answerTrue: [false, false, false, true],
+  answer: "Jim",
+};
 
 //functions
 function runTimer() {
